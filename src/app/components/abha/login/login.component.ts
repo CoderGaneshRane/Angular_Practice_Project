7841c09fa33744f95413fdbd9a75e85f7dc2af5f
GANESH SUDHIR RANE <ganesh.rane@globalspace.co.in>
2025-04-22 13:07:24 +0530

    Created login page for login using aadhar and mobile number

--- src/app/components/abha/login/login.component.ts
+++ src/app/components/abha/login/login.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  selectedLogin: 'mobile' | 'aadhaar' | null = null;
+  mobileNumber: string = '';
+  aadhaarNumber: string = '';
 
+  selectLogin(method: 'mobile' | 'aadhaar') {
+    this.selectedLogin = method;
+  }
+
+  loginWithMobile() {
+    console.log('Logging in with Mobile:', this.mobileNumber);
+  }
+
+  loginWithAadhaar() {
+    console.log('Logging in with Aadhaar:', this.aadhaarNumber);
+  }
 }
