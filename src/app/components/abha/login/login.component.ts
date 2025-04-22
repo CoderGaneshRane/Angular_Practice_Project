dec2201bcc53f65fafdbfd04bd4e07b0981f3c90
GANESH SUDHIR RANE <ganesh.rane@globalspace.co.in>
2025-04-22 14:18:40 +0530

    VAlidation added

--- src/app/components/abha/login/login.component.ts
+++ src/app/components/abha/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +11,32 @@ export class LoginComponent {
   mobileNumber: string = '';
   aadhaarNumber: string = '';
 
+  constructor(private toastr:ToastrService){
+
+  }
   selectLogin(method: 'mobile' | 'aadhaar') {
     this.selectedLogin = method;
   }
-
+  allowOnlyNumbers(event: KeyboardEvent): void {
+    const charCode = event.which ? event.which : event.keyCode;
+    if (charCode < 48 || charCode > 57) {
+      event.preventDefault();
+    }
+  }
+  
   loginWithMobile() {
+    if (!this.mobileNumber || this.mobileNumber.length !== 10 || !/^[0-9]{10}$/.test(this.mobileNumber)) {
+      this.toastr.warning('Please enter a valid 10-digit mobile number.');
+      return;
+    }
     console.log('Logging in with Mobile:', this.mobileNumber);
   }
 
   loginWithAadhaar() {
+    if (!this.aadhaarNumber || this.aadhaarNumber.length !== 12 || !/^[0-9]{12}$/.test(this.aadhaarNumber)) {
+      this.toastr.warning('Please enter a valid 12-digit Aadhaar number.');
+      return;
+    }
     console.log('Logging in with Aadhaar:', this.aadhaarNumber);
   }
 }
